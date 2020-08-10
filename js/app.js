e4eb3894a9eb32573ced7f2bacfefc3db950801d
Valentin Astudillo <gvalentin.ast@gmail.com>
2020-08-10 20:05:59 -0300

    versio n1

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/* function pedirDato() {
+   var dato = prompt('ingresa un dato');   
+} */
+
+/* function mostrarAlert (pedirDato){
+    return alert(pedirDato);
+}
+function mostrarConsola(pedirDato) {
+    return console.log(pedirDato);
+}
+function calculoCemento(numero1,numero2) {
+    return (numero1+numero2);
+}
+ var primerDato=parseInt(prompt('ingrese l de su casa'));
+ var segundoDato=parseInt(prompt('ingrese '));
+ var resultado=sumar(primerDato,segundoDato);
+
+ mostrarAlert(resultado);
+ mostrarConsola(resultado); */
