96e1f939acf9cb65494a82be27d051197ddc04ca
valennast <63021720+valennast@users.noreply.github.com>
2020-08-25 21:49:21 -0300

    version 6

--- js/app.js
+++ js/app.js
@@ -8,20 +8,13 @@ var precioMarcaVehiculo;
 var precioCoberturaVehiculo;
 
 /* Arrays a utilizar para interacturar con el html */
-var listadoTiposVehiculos=['Camioneta','Auto'];
-var listadoAñosMoldelo=[2015,2016,2017,2018,2019,2020];
-var listadoMarcaVehiculos=['Ford','Toyota','Peugeot','Volkswagen'];
-var listadoCoberturaVehiculos=['Completo','Simple','Simple con granizo','Simple con ruedas'];
-
-
-/* Este podria ser uno de las Funciones que se podria usar, cuando veamos como interactuar con el html lo cambiare */
-function menvaajePersonalizadoError(arrayDeControl){
- console.log('Debes elegir entre una de las ' + arrayDeControl.length + 'Opciones');
-}
-
+var listadoTiposVehiculos=['Elegir Tipo de Vehiculo','Camioneta','Auto'];
+var listadoAñosMoldelo=['Elegir Año',2015,2016,2017,2018,2019,2020];
+var listadoMarcaVehiculos=['Elegir Marca','Ford','Toyota','Peugeot','Volkswagen'];
+var listadoCoberturaVehiculos=['Elegir Cobertura','Completo','Simple','Simple con granizo','Simple con ruedas'];
 
 /*  Objeto   */
-class Seguro {
+class PolizaSeguro {
     constructor(tipoDeVehiculo, añoModelo, marca, cobertura) {
         this.precioTipoVehiculo = tipoDeVehiculo;
         this.precioañoModelo = añoModelo;
@@ -29,21 +22,20 @@ class Seguro {
         this.precioCoberturaVehiculo = cobertura;
     }
 }
-/* Prueba */
-crearPoliza();
 
-/* Esto debe cambiar seguramente cuando veamos como interactuar con el html */
-function completarFormulario(){
-    precioTipoVehiculo = prompt('Ingrese el tipo del vehiculo que va cotizar');
-    precioañoModelo = prompt('Ingrese el año del vehiculo que va cotizar');
-    precioMarcaVehiculo = prompt('Ingrese la marca del vehiculo que va cotizar');
-    precioCoberturaVehiculo = prompt('Ingrese la cobertura del vehiculo que va cotizar');
+/* COMPLETO LOS SELECT DEL HTML */
+
+llenarSelects(listadoMarcaVehiculos,"marcaVehiculo");
+llenarSelects(listadoCoberturaVehiculos,"coberturaVehiculo");
+llenarSelects(listadoAñosMoldelo,"añoVehiculo");
+llenarSelects(listadoTiposVehiculos,"tipoVehiculo");
+
+function llenarSelects(listadoArray,idListado){
+    var listaHtml = document.getElementById(idListado);
+    for (var i = 0; i < listadoArray.length; i++) {
+        var opcion = document.createElement("option");
+        opcion.value = listadoArray[i];
+        opcion.text = listadoArray[i];
+        listaHtml.appendChild(opcion);
+        }  
 }
-
-/* Es una funcion pero debería ser un metodo del objeto para calcular */
-function crearPoliza(){
-    completarFormulario();
-    Poliza1 = new Seguro(precioTipoVehiculo,precioMarcaVehiculo,precioMarcaVehiculo,precioCoberturaVehiculo);
-    console.log(Poliza1);
-}
-
