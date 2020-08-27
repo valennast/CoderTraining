37f99c4c3e82e741e446c388eb80eb6d0ec42a2d
valennast <63021720+valennast@users.noreply.github.com>
2020-08-27 19:12:24 -0300

    version 7

--- js/app.js
+++ js/app.js
@@ -1,17 +1,55 @@
 
 /* Variables a Utilizar */
 
-/* Variables para crear el objeto */
-var precioTipoVehiculo;
-var precioañoModelo;
-var precioMarcaVehiculo;
-var precioCoberturaVehiculo;
+/* Constantes para almacenar los precios segun tipo de vehiculos */
+
+const precioPorCamioneta= 1500;
+const precioPorAuto= 1000;
+
+/* Constantes para almacenar los precios de las marca */
+
+const precioMarcaFord = 250;
+const precioMarcaPeugeot = 300;
+const precioMarcaVolkswagen = 350;
+const precioMarcaToyota = 400;
+
+/* Constantes para almacenar los precios segun cobertura */
+
+const precioCoberturaCompleta = 250;
+const precioCoberturaSimple = 300;
+const precioCoberturaSimpleGranizo = 350;
+const precioCoberturaSimpleRuedas = 400;
 
 /* Arrays a utilizar para interacturar con el html */
+/* COMPLETO LOS SELECT DEL HTML */
+
 var listadoTiposVehiculos=['Elegir Tipo de Vehiculo','Camioneta','Auto'];
-var listadoAñosMoldelo=['Elegir Año',2015,2016,2017,2018,2019,2020];
 var listadoMarcaVehiculos=['Elegir Marca','Ford','Toyota','Peugeot','Volkswagen'];
 var listadoCoberturaVehiculos=['Elegir Cobertura','Completo','Simple','Simple con granizo','Simple con ruedas'];
+
+llenarSelects(listadoMarcaVehiculos,"marcaVehiculo");
+llenarSelects(listadoCoberturaVehiculos,"coberturaVehiculo");
+llenarSelects(listadoTiposVehiculos,"tipoVehiculo");
+
+function llenarSelects(listadoArray,idListado){
+    const listaHtml = document.getElementById(idListado);
+    for (var i = 0; i < listadoArray.length; i++) {
+        var opcion = document.createElement("option");
+        opcion.value = listadoArray[i];
+        opcion.text = listadoArray[i];
+        listaHtml.appendChild(opcion);
+        }  
+}
+/* COMPLETO LOS SELECT DE AÑOS DEL HTML (SOLO QUIERO COTIZAR LOS AUTOS CON UN MAX 5 AÑOS DE ANTIGUEDAD) */
+const max = new Date().getFullYear();
+const min = max - 5;
+const selectAñiosVehiculo = document.getElementById('añoVehiculo');
+      for(let i = max; i > min; i--) {
+           let option = document.createElement('option');
+           option.value = i;
+           option.innerHTML = i;
+           selectAñiosVehiculo.appendChild(option);
+      }      
 
 /*  Objeto   */
 class PolizaSeguro {
@@ -23,19 +61,28 @@ class PolizaSeguro {
     }
 }
 
-/* COMPLETO LOS SELECT DEL HTML */
+const formularioPoliza = document.getElementById('cotizar');
 
-llenarSelects(listadoMarcaVehiculos,"marcaVehiculo");
-llenarSelects(listadoCoberturaVehiculos,"coberturaVehiculo");
-llenarSelects(listadoAñosMoldelo,"añoVehiculo");
-llenarSelects(listadoTiposVehiculos,"tipoVehiculo");
+/* formularioPoliza.addEventListener('click', function(){
+    alert('');
+}) */
 
-function llenarSelects(listadoArray,idListado){
-    var listaHtml = document.getElementById(idListado);
-    for (var i = 0; i < listadoArray.length; i++) {
-        var opcion = document.createElement("option");
-        opcion.value = listadoArray[i];
-        opcion.text = listadoArray[i];
-        listaHtml.appendChild(opcion);
-        }  
-}
+formularioPoliza.addEventListener('click', function(){
+    
+    const vehiculo = document.getElementById('tipoVehiculo');
+    const tipoVehiculoSelect = vehiculo.options[vehiculo.selectedIndex].value;
+    
+    const año = document.getElementById('añoVehiculo');
+    const añoSelect = año.options[año.selectedIndex].value;
+    
+    const marca = document.getElementById('marcaVehiculo');
+    const marcaSelect = marca.options[marca.selectedIndex].value;
+    
+    const cobertura = document.getElementById('coberturaVehiculo');
+    const coberturaSelect = cobertura.options[cobertura.selectedIndex].value;
+    
+    /* const poliza = new PolizaSeguro(tipoVehiculoSelect,añoSelect,marcaSelect,coberturaSelect) */
+
+
+    /* alert('Se ha enviado con éxito su poliza'); */
+});
